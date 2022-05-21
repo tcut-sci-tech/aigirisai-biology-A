578d7953eb6bb0462464dae1d9290c999653b181
tcut-sci-tech <88429838+tcut-sci-tech@users.noreply.github.com>
2022-05-21 18:04:08 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,10 @@
-function FixedAnime() {
-  var headerH = $('#header').outerHeight(true);
-	var scroll = $(window).scrollTop();
-	if (scroll >= headerH){
-    $('.openbtn').addClass('fadeDown');
-    $('#header').addClass('dnone');
-  }else{
-    $('.openbtn').removeClass('fadeDown');
-    $('#header').removeClass('dnone');
-  }
-}
-
-$(window).scroll(function () {
-  FixedAnime();
-});
-
-$(window).on('load', function () {
-  FixedAnime();
-});
-
+/*---------- ヘッダー ---------- */
 $(".openbtn").click(function () {
   $(this).toggleClass('active');
-  $("#header").toggleClass('panelactive');
+  $("#g-nav").toggleClass('panelactive');
 });
 
-$("#g-navi li a").click(function () {
+$("#g-nav a").click(function () {
   $(".openbtn").removeClass('active');
-  $("#header").removeClass('panelactive');
-});
-
-$('#g-navi li a').click(function () {
-  var elmHash = $(this).attr('href');
-	var pos = $(elmHash).offset().top-0;
-	$('body,html').animate({scrollTop: pos}, 1000);
-	return false;
+  $("#g-nav").removeClass('panelactive');
 });
